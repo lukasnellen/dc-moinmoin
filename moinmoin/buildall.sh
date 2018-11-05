cae37a0680ab1d92b6b1c4c538e46a0228c42b8f
Lukas Nellen <lukas@nucleares.unam.mx>
2018-11-04 23:54:49 -0600

    Multistage build to shrink alpine-based container

--- moinmoin/buildall.sh
+++ moinmoin/buildall.sh
@@ -2,6 +2,6 @@
 
 MOINMOINVERSION=1.9.10
 
-docker build --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin:${MOINMOINVERSION} -f Dockerfile .
+docker build --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin -t lukasnellen/moinmoin:${MOINMOINVERSION} -f Dockerfile .
 docker build --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin:${MOINMOINVERSION}-debug -f Dockerfile.debug .
 docker build --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin:${MOINMOINVERSION}-slim -f Dockerfile.slim .
