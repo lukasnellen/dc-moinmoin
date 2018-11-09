71a295ced1e15217cc1991bb53256e19d32c3211
Lukas Nellen <lukas@nucleares.unam.mx>
2018-11-08 22:06:16 -0600

    Robustness of build process
    - abort on shell error
    - always pull latest base image

--- moinmoin/buildall.sh
+++ moinmoin/buildall.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
+set -e
+
 MOINMOINVERSION=1.9.10
 
-docker build --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin -t lukasnellen/moinmoin:${MOINMOINVERSION} -f Dockerfile .
-docker build --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin:${MOINMOINVERSION}-debug -f Dockerfile.debug .
-docker build --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin:${MOINMOINVERSION}-slim -f Dockerfile.slim .
+docker build --pull --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin -t lukasnellen/moinmoin:${MOINMOINVERSION} \
+       -f Dockerfile .
+docker build --pull --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin:${MOINMOINVERSION}-debug \
+       -f Dockerfile.debug .
+docker build --pull --build-arg version=${MOINMOINVERSION} -t lukasnellen/moinmoin:${MOINMOINVERSION}-slim \
+       -f Dockerfile.slim .
