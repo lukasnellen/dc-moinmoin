87119d20f50e36333f2f4de0a86b83066d559bd1
Lukas Nellen <lukas@nucleares.unam.mx>
2019-12-09 19:31:14 -0600

    Base image update and tool cleanup

--- moinmoin/entrypoint.sh
+++ moinmoin/entrypoint.sh
@@ -3,7 +3,6 @@
 export GATEWAY=$(ip route list | awk '/default/ { print $3 }')
 
 if [ -z "$*" ]; then
-#    exec gunicorn -b :8080 MoinMoin.server.wsgi:moinmoinApp
     exec gunicorn -b :8080 --forwarded-allow-ips="*" moin_wsgi
 else
     exec "$@"
